1042c1cd01d1a713d19dc24f70b3f2241ee43cba
yawei.zhang <yawei.zhang@rea-group.com>
2018-10-12 21:13:49 +0800

    Delete useless comments and files.

--- src/app/models/users.js
+++ src/app/models/users.js
@@ -5,39 +5,3 @@ module.exports = (sequelize, Sequelize) => {
         }
     })
 }
-
-
-
-// require('dotenv').config({path:__dirname+'/./../../../.env'});
-// const Sequelize = require('sequelize');
-// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-//     host: process.env.DB_HOST,
-//     dialect: 'postgres',
-//     operatorsAliases: false,
-//
-//     pool: {
-//         max: 5,
-//         min: 0,
-//         acquire: 30000,
-//         idle: 10000
-//     },
-// })
-//
-// sequelize
-//     .authenticate()
-//     .then(() => {
-//         console.log('Connection has been established successfully.');
-//     })
-//     .catch(err => {
-//         console.error('Unable to connect to the database:', err);
-//     })
-//
-// const Users = sequelize.define('users', {
-//     userName: {
-//         type: Sequelize.STRING
-//     }
-// });
-//
-// Users.findOne().then(users => {
-//     console.log(users.get('userName'));
-// });
